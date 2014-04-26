71cc87236c157e351d903af66d06233b29575996
Daniel Krikun <krikun.daniel@gmail.com>
2014-04-26 10:16:45 +0300

    Update setup.bat
    Generate icon_rc.py

--- setup.bat
+++ setup.bat
@@ -1,7 +1,11 @@
 @echo off
 rd build >nul 2>&1
 rd dist >nul 2>&1
+set PATH=%PATH%;c:\Python27\Lib\site-packages\PySide
+del /f /q icon_rc.py
+pyside-rcc -py2 -o icon_rc.py icon.qrc
 py setup.py build bdist_wininst
 cd %~dp0\build\exe.win-amd64-2.7
-rename zmq.libzmq.pyd libzmq.pyd >nul 2>&1
+copy zmq.libzmq.pyd libzmq.pyd >nul 2>&1
+del zmq.libzmq.pyd
 pause
