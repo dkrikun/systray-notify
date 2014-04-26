9f370c418a39b21ee110d3b7abe33791358d472d
Daniel Krikun <krikun.daniel@gmail.com>
2014-04-26 10:19:31 +0300

    Document setup.bat

--- setup.bat
+++ setup.bat
@@ -1,11 +1,19 @@
 @echo off
+
 rd build >nul 2>&1
 rd dist >nul 2>&1
+
+rem generate icon_rc.py
 set PATH=%PATH%;c:\Python27\Lib\site-packages\PySide
 del /f /q icon_rc.py
 pyside-rcc -py2 -o icon_rc.py icon.qrc
+
+rem run cx_freeze and create py installer
 py setup.py build bdist_wininst
+
+rem workaround pyzmq cx_freeze, see README.md
 cd %~dp0\build\exe.win-amd64-2.7
 copy zmq.libzmq.pyd libzmq.pyd >nul 2>&1
 del zmq.libzmq.pyd
+
 pause
